ccac0fec447585081be7eb835ad496294caa5e23
JIMHWANG <jimhwang1129@gmail.com>
2022-05-22 17:15:21 +0800

    Update basic BMI record CSS pattern

---
+++ js/all.js
@@ -0,0 +1,10 @@
+var HeightInput = document.querySelector('.heightInput input');
+var WeightInput = document.querySelector('.kgInput input');
+var ResultButton = document.querySelector('.BMIResultButton button');
+
+
+
+ResultButton.addEventListener('click', function (e) {
+
+    // console.log(WeightInput.value);
+}, false)
