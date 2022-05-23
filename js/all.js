2fc624e6b563c3975b3e46b29fe95ec7b4355830
JIMHWANG <jimhwang1129@gmail.com>
2022-05-23 22:23:10 +0800

    Updates colorbar dynamic color and BMI history record enhance

--- js/all.js
+++ js/all.js
@@ -3,12 +3,28 @@ function UpdateList(e) {
     var ParseLocalStorageRecord = JSON.parse(LocalStorageRecord);
     var mainContent = document.querySelector(".mainContent");
     var mainContentStr = "<h2>BMI 紀錄</h2>";
+    var ColorbarColor;
 
-    // console.log(ParseLocalStorageRecord);
+    if (ParseLocalStorageRecord == null) { return };
     Object.keys(ParseLocalStorageRecord).forEach(function (key) {
-        mainContentStr += '<div data-num=' + key + ' class="BMIRecord"><div class="colorbar"></div><div class="historyRecord"><p><em>' + ParseLocalStorageRecord[key].BMIJudgement + '</em></p><p>BMI<em>' + ParseLocalStorageRecord[key].BMIValue + '</em></p><p>weight<em>' + ParseLocalStorageRecord[key].Weight + 'kg</em ></p ><p>height<em>' + ParseLocalStorageRecord[key].Height + 'cm</em></p><p>' + ParseLocalStorageRecord[key].Date + '</p></div ></div>';
-
-        console.log(ParseLocalStorageRecord[key]);
+        switch (true) {
+            case (ParseLocalStorageRecord[key].BMIJudgement == "過輕"):
+                ColorbarColor = "#31BAF9";
+                break;
+            case (ParseLocalStorageRecord[key].BMIJudgement == "過重"):
+                ColorbarColor = "#FF982D";
+                break;
+            case (ParseLocalStorageRecord[key].BMIJudgement == "理想"):
+                ColorbarColor = "#86D73F";
+                break;
+            default:
+                ColorbarColor = "#86D73F";
+        }
+        if (key <= 9) {
+            mainContentStr += '<div data-num=' + key + ' class="BMIRecord"><div class="colorbar" style="background: ' + ColorbarColor + ';"></div><div class="historyRecord"><p><em>' + ParseLocalStorageRecord[key].BMIJudgement + '</em></p><p>BMI<em>' + ParseLocalStorageRecord[key].BMIValue + '</em></p><p>weight<em>' + ParseLocalStorageRecord[key].Weight + 'kg</em ></p ><p>height<em>' + ParseLocalStorageRecord[key].Height + 'cm</em></p><p>' + ParseLocalStorageRecord[key].Date + '</p></div ></div>';
+        } else {
+            delete ParseLocalStorageRecord[key];
+        }
     })
     mainContent.innerHTML = mainContentStr;
 }
@@ -20,7 +36,8 @@ function addRecord(e) {
     var DateStr = (AddDate.getMonth() + 1) + "-" + AddDate.getDate() + "-" + AddDate.getFullYear();
     var AddAryStr;
     var Addobj = {};
-
+    AddAry = JSON.parse(localStorage.getItem("DataSet"));
+    if (AddAry == null) { AddAry = []; }
     switch (true) {
         case (BMIValue <= 18.5):
             BMIJudgement = "過輕";
@@ -39,13 +56,14 @@ function addRecord(e) {
         "Weight": WeightInput.value,
         "Date": DateStr
     }
-    AddAry.push(Addobj);
+    AddAry.unshift(Addobj);
     AddAryStr = JSON.stringify(AddAry);
     localStorage.setItem("DataSet", AddAryStr);
     UpdateList();
 }
 
 var AddAry = [];
+
 var getLocalStorageList = localStorage.getItem('ItemList');
 var getLocalStorageListAry = JSON.parse(getLocalStorageList);
 
